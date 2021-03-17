752a75745d72de79e63f09ed644112e28a6affdc
sarah sturkie <sstur002@ucr.edu>
2021-03-17 15:11:19 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int fake_cross_validation(int data, int current_set, int feature_to_add){
+    return rand() % 100;
+}
+
+/*
+void feature_search(int data){
+    
+}*/
+
+void process_data(){
+    return;
+}
+
+int main(){
+    srand (time(NULL)); //to get random number
+
+    
+    
+    cout << endl;
+    return 0;
+}
